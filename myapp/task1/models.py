4700da3b9dd42d8ae27029bcc8d522fff56d70d4
Andrew <mahanec.andrei@gmail.com>
2024-12-09 22:06:51 +0300

    Реализовал ORM модели
    Научился мигрировать в БД созданные ORM модели

---
+++ myapp/task1/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from decimal import Decimal
+
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=70, help_text="Название игры")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, help_text="Цена")
+    size = models.DecimalField(max_digits=3, decimal_places=3, help_text="Размер файлов в GB")
+    description = models.TextField(help_text="Описание игры")
+    age_limited = models.BooleanField(default=False, help_text="Ограничение по возрасту 18+")
+    buyer = models.ManyToManyField(Buyer, related_name="games")
+
+    def __str__(self):
+        return self.title
