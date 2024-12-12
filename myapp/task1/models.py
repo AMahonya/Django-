3e0a0518581e3b9de5be1cb6fe0c6f6f6c2a7b7f
Andrew <mahanec.andrei@gmail.com>
2024-12-12 21:09:35 +0300

    cделал первые запросы в БД
    при помощи QuerySet запросов, закрепив знания о них.

--- myapp/task1/models.py
+++ myapp/task1/models.py
@@ -15,8 +15,8 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=70, help_text="Название игры")
-    cost = models.DecimalField(max_digits=10, decimal_places=2, help_text="Цена")
-    size = models.DecimalField(max_digits=3, decimal_places=3, help_text="Размер файлов в GB")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), help_text="Цена")
+    size = models.DecimalField(max_digits=6, decimal_places=3, default=Decimal('0.00'), help_text="Размер файлов в GB")
     description = models.TextField(help_text="Описание игры")
     age_limited = models.BooleanField(default=False, help_text="Ограничение по возрасту 18+")
     buyer = models.ManyToManyField(Buyer, related_name="games")
