e39aa85ec029784c57faf0d980ff4241ec9af252
Andrew <mahanec.andrei@gmail.com>
2024-12-15 16:51:26 +0300

    Настроил СУБД postgre
    сделал всё согласно тз
    использовал Django ORM для тестирования запросов в  базу данных.

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'NAzBL8_j',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- myapp/task1/models.py
+++ myapp/task1/models.py
@@ -24,7 +24,31 @@ class Game(models.Model):
     def __str__(self):
         return self.title
 
+
 class News(models.Model):
     title = models.CharField(max_length=100, help_text="Заголовок новости")
     content = models.TextField(help_text="Содержание новости")
     date = models.DateTimeField(auto_now_add=True, help_text="Дата публикации")
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, help_text="Название категории игры")
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        db_table = 'category'
+
+
+class Order(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, help_text="Игра добавленная в корзина")
+    quantity = models.IntegerField(help_text="колличество")
+    order_date = models.DateField(help_text="дата в формате гггг-мм-дд")
+    status = models.CharField(max_length=255, help_text="статус заказа")
+
+    def __str__(self):
+        return f"Order for {self.game.title} x {self.quantity}"
+
+    class Meta:
+        db_table = 'order'
